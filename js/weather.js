bf0db740413d14104a42a0ca2053732c6736e283
heyunpeng <3054830677@qq.com>
2022-11-01 20:56:17 +0800

    Site updated: 2022-11-01 20:56:17

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "4A86E8",
+    "tmpSize": "16",
+    "cityColor": "DD7E6B",
+    "citySize": "16",
+    "aqiColor": "9FC5E8",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "0px 0px 0px 0px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "center",
+    "horizontal": "center",
+    "city": "CN101180101",
+    "left": "125",
+    "top": "20",
+    "key": "c55fff264e8e41f5b74778538c686f09"
+  }
+}
